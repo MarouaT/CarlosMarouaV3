9240461ddef3b2fa542d8fc302c00b34960a1e0a
erica <ericabaggi@live.fr>
2018-10-18 08:53:56 +0200

    création du projet

---
+++ src/main/java/com/formation/Carlosmarouav3Application.java
@@ -0,0 +1,12 @@
+package com.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Carlosmarouav3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Carlosmarouav3Application.class, args);
+	}
+}
